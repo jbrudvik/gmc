c295a248e13cd71292dbeb02271c76637e650a8b
Jeremy Brudvik <jeremy@brudvik.me>
2022-06-22 15:17:26 -0400

    Add copying of module assets, better error handling

--- cli/cli.go
+++ cli/cli.go
@@ -1,13 +1,21 @@
 package cli
 
 import (
-	"errors"
+	"embed"
 	"fmt"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 const Version string = "v0.0.1"
+
+//go:embed all:assets
+var assets embed.FS
 
 func App() *cli.App {
 	return &cli.App{
@@ -19,8 +27,12 @@ func App() *cli.App {
 		ArgsUsage:       "[module]",
 		Action: func(c *cli.Context) error {
 			args := c.Args()
-			if args.Len() != 1 {
+			if args.Len() < 1 {
 				cli.ShowAppHelpAndExit(c, 0)
+				return nil
+			} else if args.Len() > 1 {
+				fmt.Fprintf(os.Stderr, "Error: Only one module name is allowed\n\n")
+				cli.ShowAppHelpAndExit(c, 1)
 				return nil
 			} else {
 				module := args.First()
@@ -30,17 +42,93 @@ func App() *cli.App {
 					errorMessage := fmt.Sprintf("Failed to create module \"%s\": %s", module, err)
 					return cli.Exit(errorMessage, 1)
 				}
+				fmt.Println("Success!")
 			}
 			return nil
 		},
 	}
 }
 
-// TODO: Factor this out to a separate module -- so we can test it well
-/*
-- If the directory already exists, don't touch it. Do throw an error.
-*/
+// TODO: Factor this out to a separate module. Then test it well.
 func CreateModule(module string) error {
-	// TODO: Implement
-	return errors.New("uh oh")
+	moduleBase := path.Base(module)
+
+	// Create module directory && change into the directory
+	err := os.Mkdir(moduleBase, 0755)
+	if err != nil {
+		return err
+	}
+	err = os.Chdir(moduleBase)
+	if err != nil {
+		return err
+	}
+
+	// Create go.mod
+	cmd := exec.Command("go", "mod", "init", module)
+	if err = cmd.Run(); err != nil {
+		return err
+	}
+
+	// Create .gitignore
+	err = os.WriteFile(".gitignore", []byte(moduleBase), 0644)
+	if err != nil {
+		return err
+	}
+
+	// Copy over assets
+	err = CopyEmbeddedFS(assets, ".")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO: Factor out to own package + Unit test
+// - Q: Can test code use its own embed? That's probably the right way to do this.
+//     - Especially if we can write to an in-memory FS (to watch that its done correctly)
+func CopyEmbeddedFS(srcFS embed.FS, dst string) error {
+	// TODO: This more robustly, especially the final line (multiple files? empty? ...)
+	entries, err := srcFS.ReadDir(".")
+	if err != nil {
+		return err
+	}
+	dir := entries[0]
+
+	root := dir.Name()
+	err = fs.WalkDir(srcFS, root, func(srcPath string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// TODO: This more slickly
+		dstPath := strings.TrimPrefix(srcPath, root)
+		if dstPath == "" {
+			return nil
+		}
+		dstPath = strings.TrimPrefix(dstPath, "/")
+
+		if entry.IsDir() {
+			// Create the dir
+			err = os.Mkdir(path.Join(dst, dstPath), 0755)
+			if err != nil {
+				return err
+			}
+		} else {
+			// Copy the file
+			fileBytes, err := fs.ReadFile(srcFS, srcPath)
+			if err != nil {
+				return err
+			}
+			err = os.WriteFile(dstPath, fileBytes, 0644)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
