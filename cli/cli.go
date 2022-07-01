d9a31c24a9b7487143ddb266c93246b4ebf386da
Jeremy Brudvik <jeremy@brudvik.me>
2022-07-01 19:57:45 -0400

    Bump to v0.0.3

--- cli/cli.go
+++ cli/cli.go
@@ -16,7 +16,7 @@ import (
 )
 
 const Name string = "gmc"
-const Version string = "v0.0.2"
+const Version string = "v0.0.3"
 const Url string = "https://github.com/jbrudvik/" + Name
 
 const Description string = "`" + Name + " [module name]` creates a directory containing:\n" +
